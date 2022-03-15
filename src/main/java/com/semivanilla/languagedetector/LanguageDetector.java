e7f921f13d72f9f4684e5eac1e6931e50da668a0
AlenGeoAlex <alengeoalex@gmail.com>
2022-03-15 17:42:56 +0530

    Initial Setup's

---
+++ src/main/java/com/semivanilla/languagedetector/LanguageDetector.java
@@ -0,0 +1,16 @@
+package com.semivanilla.languagedetector;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LanguageDetector extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
