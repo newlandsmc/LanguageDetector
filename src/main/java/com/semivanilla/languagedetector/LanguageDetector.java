8ac8a19e3a7a7dfd0dc67607b20deff71ff2340c
AlenGeoAlex <alengeoalex@gmail.com>
2022-03-15 20:59:38 +0530

    Forgot to add LanguageDetector class

--- src/main/java/com/semivanilla/languagedetector/LanguageDetector.java
+++ src/main/java/com/semivanilla/languagedetector/LanguageDetector.java
@@ -1,16 +1,31 @@
 package com.semivanilla.languagedetector;
 
+import com.semivanilla.languagedetector.handler.FileHandler;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class LanguageDetector extends JavaPlugin {
 
+    private FileHandler fileHandler;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        this.fileHandler = new FileHandler(this);
+
+        if(!this.fileHandler.initHandler()){
+            getLogger().severe("Unable to initialize a plugin file. The plugin will be disabled!");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
+
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+
+    }
+
+    public FileHandler getFileHandler() {
+        return fileHandler;
     }
 }
